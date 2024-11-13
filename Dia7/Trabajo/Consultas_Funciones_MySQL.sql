22c44991d67a9f862acb6c9c21568b7547b29757
MariaGLi <lizrazoleal27@gmail.com>
2024-11-12 22:56:52 -0500

    :construction: En proceso...

--- Dia7/Trabajo/Consultas_Funciones_MySQL.sql
+++ Dia7/Trabajo/Consultas_Funciones_MySQL.sql
@@ -51,56 +51,118 @@ select id, substring(nif, 1, 8) as Nif_Números, substring(nif, 9, 1) as Nif_Car
 -- los gastos que se han generado (columna gastos). Tenga en cuenta que en algunos casos pueden existir
 -- valores negativos. Utilice un alias apropiado para la nueva columna columna que está calculando.
 
-DELIMITER //
-create function Presupuesto_Actual (cliente_id_interno INT)
-RETURNS DECIMAL(10,2) DETERMINISTIC
-BEGIN
-	DECLARE ingreso decimal (10,2);
-    select avg (precio) into ingreso from ordenes where cliente_id_interno = cliente_id;
-    return ingreso;
-END //
-DELIMITER ;
-
-select * from departamento;
+select nombre, (presupuesto - gastos)  as Presupuesto_Actual  from departamento;
 
 -- 12. Lista el nombre de los departamentos y el valor del presupuesto actual ordenado de forma ascendente.
+
+select nombre, presupuesto from departamento order by 2 asc;
+
 -- 13. Lista el nombre de todos los departamentos ordenados de forma ascendente.
+
+select nombre from departamento order by 1 asc;
+
 -- 14. Lista el nombre de todos los departamentos ordenados de forma descendente.
 
+select nombre from departamento order by 1 desc;
+
 -- 15. Lista los apellidos y el nombre de todos los empleados, ordenados de forma
--- alfabética tendiendo en cuenta en primer lugar sus apellidos y luego su nombre.
+-- alfabética teniendo en cuenta en primer lugar sus apellidos y luego su nombre.
+
+select apellido1, apellido2, nombre from empleado order by 1 asc;
+
 -- 16. Devuelve una lista con el nombre y el presupuesto, de los 3 departamentos
 -- que tienen mayor presupuesto.
+
+select nombre, presupuesto from departamento order by 2 desc limit 3;
+
 -- 17. Devuelve una lista con el nombre y el presupuesto, de los 3 departamentos que tienen menor presupuesto.
+
+select nombre, presupuesto from departamento order by 2 asc limit 3;
+
 -- 18. Devuelve una lista con el nombre y el gasto, de los 2 departamentos que tienen mayor gasto.
+
+select nombre, gastos from departamento order by 2 desc limit 2;
+
 -- 19. Devuelve una lista con el nombre y el gasto, de los 2 departamentos que tienen menor gasto.
--- 20. Devuelve una lista con 5 filas a partir de la tercera fila de la tabla empleado. La tercera fila 
+
+select nombre, gastos from departamento order by 2 asc limit 2;
+
+-- 20. Devuelve una lista con 5 filas a partir de la tercera fila de la tabla empleado. La tercera fila
 -- se debe incluir en la respuesta. La respuesta debe incluir todas las columnas de la tabla empleado.
+
+select * from empleado;
+
 -- 21. Devuelve una lista con el nombre de los departamentos y el presupuesto, de
 -- aquellos que tienen un presupuesto mayor o igual a 150000 euros.
+
+select nombre, presupuesto from departamento where presupuesto >= 150000;
+
 -- 22. Devuelve una lista con el nombre de los departamentos y el gasto, de
 -- aquellos que tienen menos de 5000 euros de gastos.
+
+select nombre, gastos from departamento where gastos < 5000;
+
 -- 23. Devuelve una lista con el nombre de los departamentos y el presupuesto, de aquellos que tienen
 -- un presupuesto entre 100000 y 200000 euros. Sin utilizar el operador BETWEEN.
+
+select nombre, presupuesto from departamento where presupuesto > 100000 and presupuesto <  200000;
+
 -- 24. Devuelve una lista con el nombre de los departamentos que no tienen un presupuesto 
 -- entre 100000 y 200000 euros. Sin utilizar el operador BETWEEN.
+
+select nombre, presupuesto from departamento where presupuesto < 100000 or presupuesto >  200000;
+
 -- 25. Devuelve una lista con el nombre de los departamentos que tienen un
 -- presupuesto entre 100000 y 200000 euros. Utilizando el operador BETWEEN.
+
+select nombre, presupuesto from departamento where presupuesto between 100000 and 200000;
+
 -- 26. Devuelve una lista con el nombre de los departamentos que no tienen un
 -- presupuesto entre 100000 y 200000 euros. Utilizando el operador BETWEEN.
+
+select nombre, presupuesto from departamento where presupuesto not between 100000 and 200000;
+
 -- 27. Devuelve una lista con el nombre de los departamentos, gastos y presupuesto, 
 -- de aquellos departamentos donde los gastos sean mayores que el presupuesto del que disponen.
+
+select nombre, gastos, presupuesto from departamento where gastos > presupuesto;
+
 -- 28. Devuelve una lista con el nombre de los departamentos, gastos y presupuesto, 
 -- de aquellos departamentos donde los gastos sean menores que el presupuesto del que disponen.
 
+select nombre, gastos, presupuesto from departamento where gastos < presupuesto;
+
 -- 29. Devuelve una lista con el nombre de los departamentos, gastos y presupuesto, de 
 -- aquellos departamentos donde los gastos sean iguales al presupuesto del que disponen.
+
+select nombre, gastos, presupuesto from departamento where gastos = presupuesto;
+
 -- 30. Lista todos los datos de los empleados cuyo segundo apellido sea NULL.
+
+select * from empleado where apellido2 is NULL;
+
 -- 31. Lista todos los datos de los empleados cuyo segundo apellido no sea NULL.
+
+select * from empleado where apellido2 is not NULL;
+
 -- 32. Lista todos los datos de los empleados cuyo segundo apellido sea López.
+
+select * from empleado where apellido2 = 'López';
+
 -- 33. Lista todos los datos de los empleados cuyo segundo apellido
 -- sea Díaz o Moreno. Sin utilizar el operador IN.
+
+select * from empleado where apellido2 = 'Díaz' or apellido2 = 'Moreno';
+
 -- 34. Lista todos los datos de los empleados cuyo segundo apellido
 -- sea Díaz o Moreno. Utilizando el operador IN.
+
+select * from empleado where apellido2 in  ('Díaz', 'Moreno');
+
 -- 35. Lista los nombres, apellidos y nif de los empleados que trabajan en el departamento 3.
+
+select nombre, apellido1, apellido2, nif from empleado where id_departamento = 3;
+
 -- 36. Lista los nombres, apellidos y nif de los empleados que trabajan en los departamentos 2, 4 o 5.
+
+select nombre, apellido1, apellido2, nif from empleado where id_departamento in (2, 4, 5);

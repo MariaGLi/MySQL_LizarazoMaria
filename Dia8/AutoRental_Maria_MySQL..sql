e2b3773a95d57e0489b1301f664017aa7e7c3a97
MariaGLi <lizarazoleal27@gmail.com>
2024-11-14 12:51:12 -0500

    :construction: Faltan ajustes...

--- Dia8/AutoRental_Maria_MySQL..sql
+++ Dia8/AutoRental_Maria_MySQL..sql
@@ -1,7 +1,8 @@
+ DROP DATABASE IF EXISTS AutoRental_María;
  CREATE DATABASE AutoRental_María;
- 
+
  USE AutoRental_María;
- 
+
  CREATE TABLE Sucursales (
  idSucursales INT PRIMARY KEY,
  Ciudad varchar(45) NOT NULL,
@@ -13,8 +14,10 @@
  
  CREATE TABLE Empleados (
  idEmpleados INT PRIMARY KEY,
- Nombre varchar(45) NOT NULL,
- Apellido varchar(45) NOT NULL,
+ Nombre1 varchar(45) NOT NULL,
+ Nombre2 varchar(45) NOT NULL,
+ Apellido1 varchar(45) NOT NULL,
+ Apellido2 varchar(45) NOT NULL,
  Cedula INT NOT NULL,
  Direccion varchar(45) NOT NULL,
  Ciudad_Residencia varchar(45) NOT NULL,
@@ -26,8 +29,10 @@
 
 CREATE TABLE Clientes (
  idClientes INT PRIMARY KEY,
- Nombre varchar(45) NOT NULL,
- Apellido varchar(45) NOT NULL,
+ Nombre1 varchar(45) NOT NULL,
+ Nombre2 varchar(45) NOT NULL,
+ Apellido1 varchar(45) NOT NULL,
+ Apellido2 varchar(45) NOT NULL,
  Celular INT NOT NULL,
  Cedula INT NOT NULL,
  Direccion varchar(45) NOT NULL,
@@ -58,16 +63,37 @@ Fecha_Llegada DATE,
 Fecha_Esperada_Llegada DATE,
 Valor_Alquiler_Semana INT NOT NULL,
 Valor_Alquiler_Dia INT NOT NULL,
-Porcentaje_Descuento VARCHAR (45) NOT NULL,
 Valor_Cotizado INT NOT NULL,
 Valor_Pagado INT NOT NULL,
-Valor_Adicional_Retraso INT NOT NULL,
 idSucursales INT,
 idEmpleados INT,
-idClientes INT,
+idSucursal_Llegada INT,
+idSucursal_Salida INT,
 idVehiculos INT,
 FOREIGN KEY (idSucursales) REFERENCES Sucursales (idSucursales),
 FOREIGN KEY (idEmpleados) REFERENCES Empleados (idEmpleados),
-FOREIGN KEY (idClientes) REFERENCES Clientes (idClientes),
+FOREIGN KEY (idSucursal_Llegada) REFERENCES Sucursales (idSucursales),
+FOREIGN KEY (idSucursal_Salida) REFERENCES Sucursales (idSucursales),
 FOREIGN KEY (idVehiculos) REFERENCES Vehiculos (idVehiculos)
+);
+
+CREATE TABLE Retraso(
+idRetraso INT PRIMARY KEY,
+Dias_Retraso INT NOT NULL,
+Porcentaje_Dias_Retraso VARCHAR (45) NOT NULL,
+idVehiculos INT,
+IdAlquileres INT,
+FOREIGN KEY (idVehiculos) REFERENCES Vehiculos (idVehiculos),
+FOREIGN KEY (idAlquileres) REFERENCES Alquileres (idAlquileres)
+);
+
+CREATE TABLE Descuentos(
+idDescuentos INT PRIMARY KEY,
+Fecha_Inicio_Dto DATE,
+Fecha_Finalizacion_Dto DATE,
+Porcentaje_Descuento VARCHAR (45) NOT NULL,
+idClientes INT,
+idAlquileres INT,
+FOREIGN KEY (idClientes) REFERENCES Clientes (idClientes),
+FOREIGN KEY (idAlquileres) REFERENCES Alquileres (idAlquileres)
 );
